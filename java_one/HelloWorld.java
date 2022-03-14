9ed711186271df08e3cdc39fb6ff657346dd679a
960137433 <960137433@qq.com>
2022-03-14 22:20:45 +0800

    Create HelloWorld.java

---
+++ java_one/HelloWorld.java
@@ -0,0 +1,17 @@
+public class Welcome{
+	public static void main(String[] args){
+		System.out.println("hello world!");
+	}
+}
+
+/*
+
+1、cmd运行命令：javac HelloWorld.java
+运行之后会生成字节码文件：HelloWorld.class
+
+2、生成字节码文件后，cmd运行命令：java HelloWorld
+机器会打印出结果：hello world!
+
+3、修改原文件后，需要重新编译生成class文件，否则运行结果还是不变
+
+*/
